e08769fa03a8bf38d51f886df97760352bf55d01
Wellington Santos <was.sls@hotmail.com>
2026-02-03 11:39:39 -0300

    :tada: Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  // toggle faz a mesma coisa que o código comentado abaixo
+  // if (html.classList.contains("light")) {
+  //   html.classList.remove("light")
+  // } else {
+  //   html.classList.add("light")
+  // }
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatargato2.jpg")
+  } else {
+    img.setAttribute("src", "./assets/avatargato.png")
+  }
+}
